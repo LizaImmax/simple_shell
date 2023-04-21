7bde172b91977bb1dc5f7c7fd19069400b18beb5
roochieng <49204011+roochieng@users.noreply.github.com>
2023-04-21 11:40:22 +0000

    adding handlers

---
+++ path_handlers.c
@@ -0,0 +1,135 @@
+#include "simple_shell.h"
+
+
+/**
+ * get_path - Locates a command in the PATH.
+ * @command: command to locate.
+ 
+ * Return: NULL on error, otherwise - the full pathname of the command.
+ */
+char *get_path(char *command)
+{
+	char **path, *temp;
+	list_t *dirs, *head;
+	struct stat st;
+
+	path = _getenv("PATH");
+	if (!path || !(*path))
+		return (NULL);
+
+	dirs = list_paths(*path + 5);
+	head = dirs;
+
+	while (dirs)
+	{
+		temp = malloc(_strlen(dirs->dir) + _strlen(command) + 2);
+		if (!temp)
+			return (NULL);
+
+		_strcpy(temp, dirs->dir);
+		_strcat(temp, "/");
+		_strcat(temp, command);
+
+		if (stat(temp, &st) == 0)
+		{
+			free_list(head);
+			return (temp);
+		}
+
+		dirs = dirs->next;
+		free(temp);
+	}
+
+	free_list(head);
+
+	return (NULL);
+}
+
+/**
+ * copy_path - Copies path.
+ * @path: Is the list of directories.
+ *
+ * Return: A copy of path.
+ */
+char *copy_path(char *path)
+{
+	int i, len = 0;
+	char *path_copy, *pwd;
+
+	pwd = *(_getenv("PWD")) + 4;
+	for (i = 0; path[i]; i++)
+	{
+		if (path[i] == ':')
+		{
+			if (path[i + 1] == ':' || i == 0 || path[i + 1] == '\0')
+				len += _strlen(pwd) + 1;
+			else
+				len++;
+		}
+		else
+			len++;
+	}
+	path_copy = malloc(sizeof(char) * (len + 1));
+	if (!path_copy)
+		return (NULL);
+	path_copy[0] = '\0';
+	for (i = 0; path[i]; i++)
+	{
+		if (path[i] == ':')
+		{
+			if (i == 0)
+			{
+				_strcat(path_copy, pwd);
+				_strcat(path_copy, ":");
+			}
+			else if (path[i + 1] == ':' || path[i + 1] == '\0')
+			{
+				_strcat(path_copy, ":");
+				_strcat(path_copy, pwd);
+			}
+			else
+				_strcat(path_copy, ":");
+		}
+		else
+		{
+			_strncat(path_copy, &path[i], 1);
+		}
+	}
+	return (path_copy);
+}
+
+/**
+ * get_path_dir - Tokenizes a colon-separated list of
+ *                directories into a list_s linked list.
+ * @path: The colon-separated list of directories.
+ *
+ * Return: A pointer to the initialized linked list.
+ */
+list_t *list_paths(char *path)
+{
+	int indx;
+	char **dirs, *path_copy;
+	list_t *head = NULL;
+
+	path_copy = copy_path(path);
+	if (!path_copy)
+		return (NULL);
+	dirs = _strtok(path_copy, ":");
+	free(path_copy);
+	if (!dirs)
+		return (NULL);
+
+	for (indx = 0; dirs[indx]; indx++)
+	{
+		if (add_node_end(&head, dirs[indx]) == NULL)
+		{
+			free_list(head);
+			free(dirs);
+			return (NULL);
+		}
+	}
+
+	free(dirs);
+
+	return (head);
+}
---
+++ simple_shell.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <stdio.h>>
+
+
+
+int main(int argc, char **argv)
+{
+	bool prompt = true;
+
+	while (prompt)
+   	{
+      		if (argc > 1)
+      		{
+         		printf("");
+      		}
+   	}
+
+}
