1005d068ab876daa7a58c6fe3eed47b18706ba14
LizaImmax <123055305+LizaImmax@users.noreply.github.com>
2023-04-27 07:39:37 -0700

    alias

--- aliasbui.c
+++ aliasbui.c
@@ -1,5 +1,25 @@
 #include "simple_shell.h"
 
+/**
+ * print_alias - Prints the alias in the format name='value'.
+ * @alias: Pointer to an alias.
+ */
+void print_alias(alias_t *alias)
+{
+	char *alias_string;
+	int len = _strlen(alias->name) + _strlen(alias->value) + 4;
+
+	alias_string = malloc(sizeof(char) * (len + 1));
+	if (!alias_string)
+		return;
+	_strcpy(alias_string, alias->name);
+	_strcat(alias_string, "='");
+	_strcat(alias_string, alias->value);
+	_strcat(alias_string, "'\n");
+
+	write(STDOUT_FILENO, alias_string, len);
+	free(alias_string);
+
 
 /**
  * _alias - Builtin command that either prints all aliases, specific
@@ -87,26 +107,7 @@ void set_alias(char *var_name, char *value)
 		add_alias_end(&aliases, var_name, new_value);
 }
 
-/**
- * print_alias - Prints the alias in the format name='value'.
- * @alias: Pointer to an alias.
- */
-void print_alias(alias_t *alias)
-{
-	char *alias_string;
-	int len = _strlen(alias->name) + _strlen(alias->value) + 4;
 
-	alias_string = malloc(sizeof(char) * (len + 1));
-	if (!alias_string)
-		return;
-	_strcpy(alias_string, alias->name);
-	_strcat(alias_string, "='");
-	_strcat(alias_string, alias->value);
-	_strcat(alias_string, "'\n");
-
-	write(STDOUT_FILENO, alias_string, len);
-	free(alias_string);
-}
 /**
  * replace_aliases - Goes through the arguments and replace any matching alias
  * with their value.
